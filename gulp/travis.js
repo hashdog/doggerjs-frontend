c9ed389e1f0a98ecb6987574491c6158771e6bfc
Mario Colque <colquemario@gmail.com>
2015-06-05 14:00:36 -0300

    added jshint fail reporter for travis gulp task

--- gulp/travis.js
+++ gulp/travis.js
@@ -6,5 +6,6 @@ var gulp = require('gulp'),
 gulp.task('travis', function() {
   return gulp.src('app/js/**/*.js')
     .pipe(jshint())
-    .pipe(jshint.reporter('jshint-stylish'));
+    .pipe(jshint.reporter('jshint-stylish'))
+    .pipe(jshint.reporter('fail'));
 });
