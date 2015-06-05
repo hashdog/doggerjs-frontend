60b57935690bf62f6a02e84359a11e374dd6924b
Mario Colque <colquemario@gmail.com>
2015-06-05 11:55:13 -0300

    added tests

---
+++ gulp/travis.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var gulp = require('gulp'),
+    jshint = require('gulp-jshint');
+
+gulp.task('travis', function() {
+  return gulp.src('app/js/**/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'));
+});
--- gulp/watch.js
+++ gulp/watch.js
@@ -2,7 +2,7 @@
 
 var gulp = require('gulp');
 
-gulp.task('watch', ['build'] ,function () {
+gulp.task('watch', ['build'],function() {
   gulp.watch('app/styles/**/*.scss', ['styles']);
   gulp.watch('app/js/**/*.js', ['scripts']);
   gulp.watch('app/img/**/*', ['images']);
